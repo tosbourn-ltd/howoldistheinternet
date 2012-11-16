75fe42ac96b0ebf562aef904e6d3011d917a117a
Toby Osbourn <toby.osbourn@googlemail.com>
2012-11-16 23:16:35 +0000

    Added humans.txt and a favicon, check me out

--- index.php
+++ index.php
@@ -4,18 +4,29 @@
     $days       = floor(($now - $start_date)/(60*60*24));
 ?>
 <!doctype html>
+<!--
+	Hi there, cheers for checking out my source code.
+
+	It isn't the most spectacular stuff in the world but if you want
+	do make it better you can do by visiting the github page and doing
+	a pull request: https://github.com/tosbourn/howoldistheinternet
+
+	Cheers,
+	Toby (@tosbourn)
+-->
 <html>
 	<head>
+		<meta charset="utf-8">
 		<title>The Age of the Internet</title>
+		<link rel="icon" type="image/x-icon" href="/favicon.ico">
+		<link type="text/plain" rel="author" href="/humans.txt" />
 		<style type="text/css" media="all">
 			small {
 				display: block;
 				color: #ddd;
 			}
 
-			a,
-			a:visited,
-			a:active {
+			a, a:visited, a:active {
 				color: #ccc;
 				transition-property: color;
   				transition-duration: 0.5s;
@@ -25,17 +36,11 @@
 				color: #000;
 			}
 
-			p, 
-			footer {
+			p, footer {
 				width: 95%;
 				margin: 10% auto;
 				text-align: center;
 				font-size: 300%;
-			}
-
-			footer p {
-				width: 100%;
-				margin: 0 auto;
 			}
 		</style>
 	</head>
