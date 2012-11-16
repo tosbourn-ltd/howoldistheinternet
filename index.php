f6329c813d725b53240b38d0d2dc1ea24c99e462
Toby Osbourn <toby.osbourn@googlemail.com>
2012-11-16 22:19:55 +0000

    Clarified the date choice - added some simple styling

--- index.php
+++ index.php
@@ -7,13 +7,48 @@
 <html>
 	<head>
 		<title>The Age of the Internet</title>
+		<style type="text/css" media="all">
+			small {
+				display: block;
+				color: #ddd;
+			}
+
+			a,
+			a:visited,
+			a:active {
+				color: #ccc;
+				transition-property: color;
+  				transition-duration: 0.5s;
+			}
+
+			a:hover {
+				color: #000;
+			}
+
+			p, 
+			footer {
+				width: 95%;
+				margin: 10% auto;
+				text-align: center;
+				font-size: 300%;
+			}
+
+			footer p {
+				width: 100%;
+				margin: 0 auto;
+			}
+		</style>
 	</head>
+
 	<body>
 		<p>
-			The Internet is <?=$days; ?> days Old.
+			The Internet is <?=$days; ?> days Old.<sup>*</sup>
+		</p>
+		<p>
+			What have you done today to make it better?
 		</p>
 		<footer>
-			<p>What have you done today to make it better?</p>
+			<small><sup>*</sup>Assuming by Internet we mean the Internet as we know it today (<a href="http://en.wikipedia.org/wiki/History_of_the_World_Wide_Web">the World Wide Web</a>)</small>
 			<small>Made by <a href="https://www.twitter.com/tosbourn" rel="author">Toby</a>.</small>
 		</footer>
 	</body>
