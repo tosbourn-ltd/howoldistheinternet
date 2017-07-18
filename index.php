75c755def977cd0fa13c7f4bcf09b05c5a3d2136
kittysquee <kittysquee@gmail.com>
2017-07-18 11:19:14 +0100

    Adds carbon ads to site

--- index.php
+++ index.php
@@ -57,6 +57,15 @@
       <a class="social linkedin" target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=http%3A%2F%2Fhowoldistheinter.net&amp;title=The+internet+is+<?=$web_days?>+days+old&amp;summary=The+internet+is+<?=$web_days?>+days+old&amp;source=http%3A%2F%2Fhowoldistheinter.net">LinkedIn</a>
     </section>
 
+    <aside class="ads">
+      <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=howoldistheinternet" id="_carbonads_js"></script>
+    </aside>
+<!--
+
+    <div class="ad-box">
+      <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=howoldistheinternet" id="_carbonads_js"></script>
+    </div> -->
+
     <section class="intro">
       <div class="container">
         <h1>Internet Timeline &darr;</h1>
